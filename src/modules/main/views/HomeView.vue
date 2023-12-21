69e862f3b79b0f96198cac4377a0890829de2377
Luiz Humberto <luizhumberto@gmail.com>
2023-12-21 16:41:43 -0300

    feat: create a home carto to link password home module

--- src/modules/main/views/HomeView.vue
+++ src/modules/main/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts"></script>
 <template>
   <v-container class="d-flex justify-center fill-height">
-    <v-responsive
-      class="text-center"
-      max-width="250"
-    >
+    <v-responsive max-width="250">
+      <v-card
+        :to="{ name: 'PasswordHome' }"
+        variant="outlined"
+      >
+        <v-card-title>Password Generator</v-card-title>
+        <v-card-text class="text-justify"
+          >Gere passwords seguras e difíceis de ser quebradas</v-card-text
+        >
+      </v-card>
     </v-responsive>
   </v-container>
 </template>
