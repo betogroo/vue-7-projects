53cc0bd63259bf6d53957598c4f7eea7eefca832
Luiz Humberto <luizhumberto@gmail.com>
2023-12-21 17:09:18 -0300

    feat: create lenght field

--- src/modules/password/views/PasswordHome.vue
+++ src/modules/password/views/PasswordHome.vue
@@ -1,11 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+//reactive
+const length = ref<number>(6)
+
+//methods
+const decrement = () => {
+  length.value--
+}
+const increment = () => {
+  length.value++
+}
+
+//computed
+const color = computed(() => {
+  //refactor
+  if (length.value < 8) return 'red'
+  if (length.value > 10) return 'teal'
+  if (length.value > 12) return 'green'
+  if (length.value > 16) return 'orange'
+  return 'blue'
+})
+</script>
 <template>
-  <v-container class="d-flex justify-center fill-height">
-    <v-responsive
-      class="text-center"
-      max-width="250"
-    >
-      <h1>Password Generator</h1>
-    </v-responsive>
+  <v-container class="d-flex flex-column justify-center">
+    <h1>Password Generator</h1>
+    <v-row>
+      <v-col>
+        <v-slider
+          v-model="length"
+          :color="color"
+          max="32"
+          min="1"
+          :step="1"
+          track-color="grey"
+        >
+          <template #prepend>
+            <h3>Tamanho da Senha</h3>
+          </template>
+          <template #append>
+            <v-text-field
+              v-model="length"
+              density="compact"
+              hide-details
+              single-line
+              style="width: 50px"
+              variant="outlined"
+            ></v-text-field>
+          </template>
+        </v-slider>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
