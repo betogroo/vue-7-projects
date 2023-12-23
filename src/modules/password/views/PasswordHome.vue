4f734c258479471f70c629d036fafaf129426bb2
betogroo <luizhumberto@gmail.com>
2023-12-23 18:42:27 -0300

    refactor: security color schema

--- src/modules/password/views/PasswordHome.vue
+++ src/modules/password/views/PasswordHome.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { AppSwitch, AppSlider } from '../components'
+import { reactive } from 'vue'
 
 //reactive
-const length = ref<number>(6)
-const includeUppercase = ref<boolean>(false)
-const includeNumber = ref<boolean>(false)
-const includeSpecialCharacters = ref<boolean>(false)
+
+const formData = reactive({
+  length: 6,
+  includeUppercase: false,
+  includeNumber: false,
+  includeSpecialCharacters: false,
+})
 
 //methods
 const toggleUppercase = () => {
-  includeUppercase.value = !includeUppercase.value
+  formData.includeUppercase = !formData.includeUppercase
 }
 const toggleNumber = () => {
-  includeNumber.value = !includeNumber.value
+  formData.includeNumber = !formData.includeNumber
 }
 const toggleSpecialCharacters = () => {
-  includeSpecialCharacters.value = !includeSpecialCharacters.value
+  formData.includeSpecialCharacters = !formData.includeSpecialCharacters
 }
 const handleSubmit = () => {
-  console.log(includeUppercase.value)
+  console.log(formData)
 }
 
 const securityColor = computed(() => {
   const countTrue = [
-    includeUppercase.value,
-    includeNumber.value,
-    includeSpecialCharacters.value,
+    formData.includeUppercase,
+    formData.includeNumber,
+    formData.includeSpecialCharacters,
   ].filter(Boolean).length
   let color
 
@@ -46,38 +50,53 @@ const securityColor = computed(() => {
 </script>
 <template>
   <v-container class="d-flex flex-column justify-center">
-    <h1>Password Generator</h1>
-    <v-form @submit.prevent="handleSubmit">
-      <v-row align="center">
-        <v-col>
-          <AppSlider v-model="length" />
-        </v-col>
-      </v-row>
-      <v-row align="center">
-        <v-col>
-          <AppSwitch
-            v-model="includeUppercase"
-            :color="securityColor"
-            title="Incluir Letra Maiúscula"
-            @toggle-switch="toggleUppercase()"
-          />
-          <AppSwitch
-            :color="securityColor"
-            title="Incluir Número"
-            @toggle-switch="toggleNumber()"
-          />
-          <AppSwitch
-            :color="securityColor"
-            title="Incluir Caracteres Especiais"
-            @toggle-switch="toggleSpecialCharacters()"
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col><v-btn type="submit">Enviar</v-btn></v-col>
-      </v-row>
-    </v-form>
+    <h1 class="text-center">Password Generator</h1>
+    <v-card
+      class="pa-4"
+      variant="outlined"
+    >
+      <v-form @submit.prevent="handleSubmit">
+        <v-row align="center">
+          <v-col>
+            <AppSlider v-model="formData.length" />
+          </v-col>
+        </v-row>
+        <v-row align="center">
+          <v-col>
+            <AppSwitch
+              v-model="formData.includeUppercase"
+              :color="securityColor"
+              title="Incluir Letra Maiúscula"
+              @toggle-switch="toggleUppercase()"
+            />
+            <AppSwitch
+              v-model="formData.includeNumber"
+              :color="securityColor"
+              title="Incluir Número"
+              @toggle-switch="toggleNumber()"
+            />
+            <AppSwitch
+              v-model="formData.includeSpecialCharacters"
+              :color="securityColor"
+              title="Incluir Caracteres Especiais"
+              @toggle-switch="toggleSpecialCharacters()"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            ><v-btn
+              block
+              :color="securityColor"
+              type="submit"
+              >Gerar Senha</v-btn
+            ></v-col
+          >
+        </v-row>
+      </v-form>
+    </v-card>
 
-    {{ includeUppercase }}, {{ includeNumber }}, {{ includeSpecialCharacters }}
+    {{ formData.includeUppercase }}, {{ formData.includeNumber }},
+    {{ formData.includeSpecialCharacters }}
   </v-container>
 </template>
