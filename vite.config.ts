d3f5d73ff2c6602c58f2f68ab659dcf8b926f12a
betogroo <luizhumberto@gmail.com>
2023-12-28 09:34:14 -0300

    chore: configure pwa files

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,40 @@ export default defineConfig({
         configFile: 'src/styles/settings.scss',
       },
     }),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Vue 7 Projects',
+        short_name: 'Vue 7 Projects',
+        description: 'A 7 app examples to learn Vue 3',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
     ViteFonts({
       google: {
         families: [
