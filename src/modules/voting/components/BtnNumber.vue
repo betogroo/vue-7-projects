5c1cf90d442ac7a8e8076a11a0eba10cb33711c6
betogroo <luizhumberto@gmail.com>
2023-12-29 17:27:12 -0300

    feat(incomplete): display and keyboard

---
+++ src/modules/voting/components/BtnNumber.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  value: number | string
+}
+const props = defineProps<Props>()
+
+const $emit = defineEmits<{
+  handleClick: [value: number | string]
+}>()
+
+const handleClick = () => {
+  $emit('handleClick', props.value)
+}
+</script>
+
+<template>
+  <v-btn
+    class="ma-2"
+    color="blue-grey-lighten-2"
+    width="100"
+    @click.prevent="handleClick"
+  >
+    <template #default>
+      <h1>{{ value }}</h1>
+    </template>
+  </v-btn>
+</template>
--- src/modules/voting/views/VotingHome.vue
+++ src/modules/voting/views/VotingHome.vue
@@ -1,6 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue'
+import { BtnNumber } from '../components'
+
+const btn = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+const display = ref('')
+
+const updateDisplay = (value: number | string) => {
+  if (display.value === '0') display.value = ''
+  display.value += value
+}
+
+const suitorCard = computed<boolean>(() => display.value.length === 3)
+</script>
 <template>
   <v-container class="d-flex justify-center fill-height">
-    Voting System
+    <v-card>
+      <v-row
+        v-for="row in 3"
+        :key="row"
+        no-gutters
+      >
+        <v-col
+          v-for="col in 3"
+          :key="col"
+          :order="col === 1 ? 'last' : col === 3 ? 'first' : ''"
+        >
+          <BtnNumber
+            :disabled="suitorCard"
+            :value="btn[(row - 1) * 3 + col - 1]"
+            @handle-click="updateDisplay"
+          />
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col class="text-center"
+          ><BtnNumber
+            :disabled="suitorCard"
+            :value="0"
+            @handle-click="updateDisplay"
+        /></v-col>
+      </v-row>
+    </v-card>
+    <v-card v-if="suitorCard">
+      <v-alert>{{ display }}</v-alert>
+    </v-card>
+
+    {{ display }}
   </v-container>
 </template>
