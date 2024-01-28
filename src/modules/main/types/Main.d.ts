f088dff5406f4eab430d15790555cf3bf6350716
betogroo <luizhumberto@gmail.com>
2024-01-28 11:58:36 -0300

    refactor(temp): app param type

--- src/modules/main/types/Main.d.ts
+++ src/modules/main/types/Main.d.ts
@@ -1,9 +1,5 @@
-import { z } from 'zod'
-
-export const AppSchema = z.object({
-  title: z.string().min(1, 'Campo Obrigatório'),
-  description: z.string().min(1, 'Campo Obrigatório'),
-  name: z.string().min(1, 'Campo Obrigatório'),
-})
-
-export type App = z.infer<typeof AppSchema>
+export interface AppParam {
+  title: string
+  description: string
+  name: { name: string; params?: { id: number | string } }
+}
