14891f80ccee85f777cdf1f4bdbcff6b7a97a4c4
betogroo <luizhumberto@gmail.com>
2024-07-12 10:29:48 -0300

    feat: create gird cell

---
+++ src/modules/queensGame/components/GridCell.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { GridContent } from '../types/QueensGame'
+interface Props {
+  content?: GridContent
+  color?: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <v-sheet
+    border="opacity-100 sm"
+    class="d-flex align-center justify-center pa-1 cursor-pointer"
+    :color="color"
+    height="28"
+    max-width="28"
+    width="28"
+    ><v-img
+      v-if="content === 'queen'"
+      src="@/assets/queens_imgs/crown.png"
+    ></v-img>
+    <v-icon
+      v-if="content === 'marked'"
+      size="x-small"
+      >mdi-circle</v-icon
+    >
+  </v-sheet>
+</template>
