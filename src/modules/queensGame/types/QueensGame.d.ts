f6bb02f3265c66c3e2aef77c66a2e5196c6379bf
betogroo <luizhumberto@gmail.com>
2024-07-12 08:28:10 -0300

    feat: create queens type file

--- src/modules/queensGame/types/QueensGame.d.ts
+++ src/modules/queensGame/types/QueensGame.d.ts
@@ -1,15 +1,5 @@
 import { z } from 'zod'
 
-const colorsSchema = z.enum(['error', 'warning', 'success'])
-const messagesSchema = z.enum(['fraca', 'razoável', 'forte'])
-const strongSchema = z.boolean().nullish()
+const gridContentSchema = z.enum(['queen', 'marked', ''])
 
-export const securityLevelSchema = z.object({
-  color: z.infer<typeof colorsSchema>,
-  message: z.infer<typeof messagesSchema>,
-  strong: z.infer<typeof strongSchema>,
-})
-
-export type colors = z.infer<typeof colorsSchema>
-
-export type SecurityLevel = z.infer<typeof securityLevelSchema>
+export type GridContent = z.infer<typeof gridContentSchema>
